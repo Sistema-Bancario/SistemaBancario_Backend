da5888ffd1673391aa26ddf96cbdf5cfe6b69f7a
Jefrey Lopez <jlopez-2018422@kinal.edu.gt>
2023-06-19 21:48:48 -0600

    Crear funcion de transferir por favoritos, su route, modificacion en el modelo

--- routes/transactions.js
+++ routes/transactions.js
@@ -2,16 +2,20 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 const { validarCampos } = require("../middlewares/validar-campos");
 const { validarJWT } = require("../middlewares/validar-jwt");
-const { transferir, mostrarTransaccionesPorNumeroCuenta } = require('../controllers/transaction');
+const { transferir, mostrarTransaccionesPorNumeroCuenta, transferirCuentaDestino } = require('../controllers/transaction');
 
 const router = Router();
 
 router.post('/hacerTransaccion', [
-  //validarJWT,
+  validarJWT,
   check('monto', 'Monto obligatorio').not().isEmpty(),
   check('cuentaDestino', 'Destino obligatorio').not().isEmpty(),
   validarCampos
 ], transferir);
+
+router.post('/transferirFav/:cuentaDestino', [
+  validarJWT,
+], transferirCuentaDestino);
 
 router.get('/vertransacciones/:numeroCuenta',[
 ],mostrarTransaccionesPorNumeroCuenta)
